4ac904dd40c8d964c18903194663c808cb21596d
MinhDuon9 <minhdc980@gmail.com>
2021-12-16 08:12:13 +0700

    Create_Table_DCM618

--- DuongCongMinh618/Models/UniversityDCM618.cs
+++ DuongCongMinh618/Models/UniversityDCM618.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DuongCongMinh618.Models
 {
     public class UniversityDCM618
     {
         [Key]
+        [DisplayName("Mã định danh")]
         public int UniversityId { get; set; }
+        [DisplayName("Tên trường")]
         public string UniversityName { get; set; }
     }
 }
