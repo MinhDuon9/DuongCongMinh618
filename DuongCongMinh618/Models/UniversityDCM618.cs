77f28be7dd813d858fd79ed2091ced3bc2c12182
MinhDuon9 <minhdc980@gmail.com>
2021-12-16 07:51:55 +0700

    Create_Table_University_DCM618

--- DuongCongMinh618/Models/UniversityDCM618.cs
+++ DuongCongMinh618/Models/UniversityDCM618.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DuongCongMinh618.Models
 {
     public class UniversityDCM618
     {
+        [Key]
+        public int UniversityId { get; set; }
+        public string UniversityName { get; set; }
     }
 }
